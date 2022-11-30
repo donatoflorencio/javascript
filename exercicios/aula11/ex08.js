21f5073096789f2fd357b51c2defeebbc7264711
donatoflorencio <donato.barros2014@gmail.com>
2022-11-29 23:42:36 -0300

    js variaveis

---
+++ exercicios/aula11/ex08.js
@@ -0,0 +1,6 @@
+var vel = 12
+console.log(`A velociade do seu carro é ${vel} km/hr`)
+if (vel > 60) {
+  console.log(`Você ultrapassou a velocidade maxima. MULTADO!`)
+}
+console.log(`dirija sempre com sinto de segurança!`)
