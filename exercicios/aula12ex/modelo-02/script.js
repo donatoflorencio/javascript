02a7f2376c98848854fd260bc3293155c2d13318
donatoflorencio <donato.barros2014@gmail.com>
2022-12-05 12:17:17 -0300

    fim

--- exercicios/aula12ex/modelo-02/script.js
+++ exercicios/aula12ex/modelo-02/script.js
@@ -13,28 +13,37 @@ function verificar() {
     img.setAttribute('id', 'foto')
     if (fsex[0].checked) {
       gênero = 'Homem'
-      if ( idade >= 0 && idade > 10) {
+      if ( idade >= 0 && idade < 10) {
         //criança
+        img.setAttribute('src', 'imagens/foto-bebe-m.png')
      } else if (idade < 21){
        //jovem
+       img.setAttribute('src', 'imagens/foto-jovem-m.png')
      } else if (idade < 50) {
        //adulto
+       img.setAttribute('src', 'imagens/foto-adulto-m.png')
      } else {
        //idoso
+       img.setAttribute('src', 'imagens/foto-idoso-m.png')
      }
     } else if (fsex[1].checked) {
       gênero = 'Mulher'
-      if ( idade >= 0 && idade > 10) {
+      if ( idade >= 0 && idade < 10) {
         //criança
+        img.setAttribute('src', 'imagens/foto-bebe-f.png')
      } else if (idade < 21){
        //jovem
+       img.setAttribute('src', 'imagens/foto-jovem-f.png')
      } else if (idade < 50) {
        //adulto
+       img.setAttribute('src', 'imagens/foto-adulto-f.png')
      } else {
        //idoso
+       img.setAttribute('src', 'imagens/foto-idoso-f.png')
      }
     }
     res.style.textAlign = 'center'
     res.innerHTML = `Detectamos ${gênero} com ${idade} anos.`
+    res.appendChild(img)
   }
 }
