f1577779f85ca2800dc18ef8677b17f0e7aaba97
donatoflorencio <donato.barros2014@gmail.com>
2022-12-26 21:43:30 -0300

    consegui

--- teste/index.js
+++ teste/index.js
@@ -1,8 +1,18 @@
 const btnMenu = document.getElementById('btn-menu');
 
-function toggleMenu() {
+function toggleMenu(event) {
+  if (event.type === 'touchstart') event.preventDefault()
   const nav = document.getElementById('nav');
-  nav.classList.toggle('active')
+  nav.classList.toggle('active');
+  const active = nav.classList.contains('active')
+  event.currentTarget.setAttribute('aria-expanded', active)
+  if (active) {
+    event.currentTarget.setAttribute('aria-label', 'Fechar Menu')
+  } else {
+    event.currentTarget.setAttribute('aria-label', 'Abrir Menu')
+
+  }
 }
 
 btnMenu.addEventListener('click', toggleMenu);
+btnMenu.addEventListener('touchstart', toggleMenu);
---
+++ teste/teste.js
@@ -0,0 +1,20 @@
+let btnMenu = document.getElementById('btn-menu')
+let nav = document.getElementById('nav')
+
+function toggleMenu() {
+  nav.classList.toggle('active')
+}
+
+
+btnMenu.addEventListener('click', toggleMenu)
+
+
+
+// const btnMenu = document.getElementById('btn-menu');
+// const nav = document.getElementById('nav');
+
+// function toggleMenu() {
+//   nav.classList.toggle('active');
+// }
+
+// btnMenu.addEventListener('click', toggleMenu);
