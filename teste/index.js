6f36de9279a7f019920c5d1015c7d30855945578
donatoflorencio <donato.barros2014@gmail.com>
2023-01-04 02:13:07 -0300

    o teste ficou top

--- teste/index.js
+++ teste/index.js
@@ -1,1 +1,20 @@
+let btn = document.querySelector(".menu-burguer")
+let menu = document.getElementById("menu")
+let menushow = false;
 
+
+
+btn.addEventListener("click", function () {
+  menushow = !menushow
+
+  if(menushow) {
+    menu.style.left = "-1000px"
+
+    menu.style.animationName = "menushow"
+
+  }else {
+    menu.style.left = "0px"
+
+    menu.style.animationName = ""
+  }
+})
