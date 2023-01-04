90b52104ac86d4c84af3f14ad55f83b4f04e8e62
donatoflorencio <donato.barros2014@gmail.com>
2023-01-03 21:06:53 -0300

    termino dps

--- teste/index.js
+++ teste/index.js
@@ -1,15 +1,1 @@
-let nav = document.getElementById('nav')
-let btn = document.getElementById('btn-menu')
-let showSidebar = false; /*keyfreme*/
-
-function Sidebar() {
-  showSidebar = !showSidebar;
-
-  if(showSidebar) {
-    nav.style.left = "0vw"
-    nav.style.animationName = "showSidebar"
-  } else {
-    nav.style.left = "-100vw"
-  }
-}
 
